0e4820d8202ad113e43c8169c25f1fb0a24bcc57
Phumrapee Limpianchop <git@rayriffy.com>
2023-11-12 02:20:53 +0900

    feat: stable page

--- src/packlets/data/index.ts
+++ src/packlets/data/index.ts
@@ -5,10 +5,11 @@ import { bkkjs18 } from './archive/18'
 
 import type { Active, Archive } from './types'
 
-export const active: Active = {
-  number: 19,
-  date: dayjs('2024-01-27'),
-  links: {},
-}
+export const active: Active = null
+// export const active: Active = {
+//   number: 19,
+//   date: dayjs('2024-01-27'),
+//   links: {},
+// }
 
 export const archives: Archive[] = [bkkjs18, bkkjs17]
