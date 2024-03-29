f3bdb9292570b70b7df2da55f73c17fc1798ddeb
Phumrapee Limpianchop <git@rayriffy.com>
2024-03-29 13:35:00 +0700

    feat: 19 data

---
+++ src/packlets/data/archive/19.ts
@@ -0,0 +1,12 @@
+import dayjs from 'dayjs'
+
+import type { Archive } from '$data/types'
+
+export const bkkjs19: Archive = {
+  number: 19,
+  date: dayjs('2024-01-27'),
+  links: {
+    event: 'https://creatorsgarten.org/event/bkkjs19',
+    playlistId: 'PLTuz2sLvbRpy3Gp7EXD8Vqz6vCMBdNF0N',
+  },
+}
--- src/packlets/data/index.ts
+++ src/packlets/data/index.ts
@@ -2,19 +2,10 @@ import dayjs from 'dayjs'
 
 import { bkkjs17 } from './archive/17'
 import { bkkjs18 } from './archive/18'
+import { bkkjs19 } from './archive/19'
 
 import type { Active, Archive } from './types'
 
-export const active: Active = {
-  number: 19,
-  date: dayjs('2024-01-27'),
-  links: {
-    cfp: {
-      link: 'https://grtn.org/e/bkkjs19/cfp',
-      until: dayjs('2023-12-13'),
-    },
-    event: 'https://www.eventpop.me/e/16880',
-  },
-}
+export const active: Active = null
 
-export const archives: Archive[] = [bkkjs18, bkkjs17]
+export const archives: Archive[] = [bkkjs19, bkkjs18, bkkjs17]
