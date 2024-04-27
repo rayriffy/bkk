c04e134946b4d2591419d9311b73de958bee9b89
Phumrapee Limpianchop <git@rayriffy.com>
2024-04-27 16:56:37 +0900

    feat: bkkjs 20

--- src/packlets/data/index.ts
+++ src/packlets/data/index.ts
@@ -6,6 +6,16 @@ import { bkkjs19 } from './archive/19'
 
 import type { Active, Archive } from './types'
 
-export const active: Active = null
+export const active: Active = {
+  number: 20,
+  date: dayjs('2024-05-18'),
+  links: {
+    cfp: {
+      link: 'https://www.eventpop.me/events/26956-bkkjs20/application_forms/1844/applicants/new',
+      until: dayjs('2024-04-30'),
+    },
+    event: 'https://www.eventpop.me/e/26956/bkkjs20',
+  },
+}
 
 export const archives: Archive[] = [bkkjs19, bkkjs18, bkkjs17]
