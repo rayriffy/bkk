48e1cd69658c2182a6a8632785eae7607feb91fe
Phumrapee Limpianchop <git@rayriffy.com>
2025-01-28 23:03:02 +0700

    Create 21.ts

---
+++ src/packlets/data/archive/21.ts
@@ -0,0 +1,12 @@
+import dayjs from 'dayjs'
+
+import type { Archive } from '$data/types'
+
+export const bkkjs21: Archive = {
+  number: 21,
+  date: dayjs('2024-09-14'),
+  links: {
+    event: 'https://creatorsgarten.org/event/bkkjs21',
+    playlistId: 'PLTuz2sLvbRpxkhdEH2U4Hj1kfHyCMzRRZ',
+  },
+}
